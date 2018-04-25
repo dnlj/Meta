b83bd79805d3bb89fab9a308c63773f056f6e820
DanielJ <danielwjameson@yahoo.com>
2018-04-24 20:40:17 -0500

    Correct whitespace

--- include/Meta/TypeSet/Concat.hpp
+++ include/Meta/TypeSet/Concat.hpp
@@ -1,37 +1,37 @@
 #pragma once
 
 namespace Meta::TypeSet {
-		/**
-		 * Concatenates the type lists of the types in @p Types.
-		 * @tparam Types The list of types.
-		 */
-		template<class... Types>
-		struct Concat;
+	/**
+	 * Concatenates the type lists of the types in @p Types.
+	 * @tparam Types The list of types.
+	 */
+	template<class... Types>
+	struct Concat;
 
-		/** @see Concat */
-		template<
-			template<class...> class Set,
-			class... Types
-		> struct Concat<Set<Types...>> {
-			using type = Set<Types...>;
-		};
+	/** @see Concat */
+	template<
+		template<class...> class Set,
+		class... Types
+	> struct Concat<Set<Types...>> {
+		using type = Set<Types...>;
+	};
 
-		/** @see Concat */
-		template<
-			template<class...> class Set,
-			class... Types1,
-			class... Types2
-		> struct Concat<Set<Types1...>, Set<Types2...>> {
-			using type = Set<Types1..., Types2...>;
-		};
+	/** @see Concat */
+	template<
+		template<class...> class Set,
+		class... Types1,
+		class... Types2
+	> struct Concat<Set<Types1...>, Set<Types2...>> {
+		using type = Set<Types1..., Types2...>;
+	};
 
-		/** @see Concat */
-		template<class Set1, class Set2, class Set3, class... Sets>
-		struct Concat<Set1, Set2, Set3, Sets...>
-			: Concat<
-				typename Concat<Set1, Set2>::type,
-				Set3,
-				Sets...
-			> {
-		};
+	/** @see Concat */
+	template<class Set1, class Set2, class Set3, class... Sets>
+	struct Concat<Set1, Set2, Set3, Sets...>
+		: Concat<
+			typename Concat<Set1, Set2>::type,
+			Set3,
+			Sets...
+		> {
+	};
 }
--- include/Meta/TypeSet/MakeUnique.hpp
+++ include/Meta/TypeSet/MakeUnique.hpp
@@ -9,29 +9,29 @@
 
 
 namespace Meta::TypeSet {
-		/**
-		 * Removes duplicate types from the type list of @p Set.
-		 * @tparam Set The type.
-		 * @tparam I The index sequence used internally. This should be left empty.
-		 */
-		template<class Set, class I = void>
-		struct MakeUnique;
+	/**
+	 * Removes duplicate types from the type list of @p Set.
+	 * @tparam Set The type.
+	 * @tparam I The index sequence used internally. This should be left empty.
+	 */
+	template<class Set, class I = void>
+	struct MakeUnique;
 		
-		/** @see MakeUnique */
-		template<template<class...> class Set, class... Types>
-		struct MakeUnique<Set<Types...>, void>
-			: MakeUnique<Set<Types...>, std::index_sequence_for<Types...>> {
-		};
+	/** @see MakeUnique */
+	template<template<class...> class Set, class... Types>
+	struct MakeUnique<Set<Types...>, void>
+		: MakeUnique<Set<Types...>, std::index_sequence_for<Types...>> {
+	};
 
-		/** @see MakeUnique */
-		template<template<class...> class Set, class... Types, size_t... Indices>
-		struct MakeUnique<Set<Types...>, std::index_sequence<Indices...>>
-			: Concat<
-				typename std::conditional<
-					IndexOf<Types, Types...>::value == Indices,
-					Set<Types>,
-					Set<>
-				>::type...
-			> {;
-		};
+	/** @see MakeUnique */
+	template<template<class...> class Set, class... Types, size_t... Indices>
+	struct MakeUnique<Set<Types...>, std::index_sequence<Indices...>>
+		: Concat<
+			typename std::conditional<
+				IndexOf<Types, Types...>::value == Indices,
+				Set<Types>,
+				Set<>
+			>::type...
+		> {;
+	};
 }
