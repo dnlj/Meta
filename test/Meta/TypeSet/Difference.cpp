179cdf945293ddb3fdbc16b286e2e376046eb60b
DanielJ <danielwjameson@yahoo.com>
2018-04-27 22:31:04 -0500

    Update Test::* utility functions for better debugging

--- test/Meta/TypeSet/Difference.cpp
+++ test/Meta/TypeSet/Difference.cpp
@@ -16,106 +16,133 @@
 namespace {
 	template<template<class...> class SetType>
 	void twoTest() {
-		// Empty
-		Test::binaryAssert<
-			Meta::TypeSet::Difference,
+		{ // Empty
+			constexpr auto value = Test::checkBinaryTypeOperator<
+				Meta::TypeSet::Difference,
 			
-			SetType<>,
-			SetType<>,
+				SetType<>,
+				SetType<>,
 
-			SetType<>
-		>();
+				SetType<>
+			>();
 
-		// All same types
-		Test::binaryAssert<
-			Meta::TypeSet::Difference,
+			ASSERT_TRUE(value);
+		}
+
+		{ // All same types
+			constexpr auto value = Test::checkBinaryTypeOperator<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
-			SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
 
-			SetType<>
-		>();
+				SetType<>
+			>();
 
-		// Some same types
-		Test::binaryAssert<
-			Meta::TypeSet::Difference,
+			ASSERT_TRUE(value);
+		}
+
+		{ // Some same types
+			constexpr auto value = Test::checkBinaryTypeOperator<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
-			SetType<Type<3>, Type<6>, Type<1>, Type<8>>,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
+				SetType<Type<3>, Type<6>, Type<1>, Type<8>>,
 
-			SetType<Type<2>, Type<4>>
-		>();
+				SetType<Type<2>, Type<4>>
+			>();
 
-		// None same types
-		Test::binaryAssert<
-			Meta::TypeSet::Difference,
+			ASSERT_TRUE(value);
+		}
+
+		{ // None same types
+			constexpr auto value = Test::checkBinaryTypeOperator<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
-			SetType<Type<5>, Type<6>, Type<7>, Type<8>>,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>,
+				SetType<Type<5>, Type<6>, Type<7>, Type<8>>,
 
-			SetType<Type<1>, Type<2>, Type<3>, Type<4>>
-		>();
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>();
+
+			ASSERT_TRUE(value);
+		}
 	}
 
 	template<template<class...> class SetType>
 	void multipleTest() {
-		// Empty
-		Test::multipleBinaryAssert<
-			Meta::TypeSet::Difference,
+		{ // Empty
+			constexpr auto value = Test::checkBinaryTypeOperatorMultiple<
+				Meta::TypeSet::Difference,
 			
-			SetType<>,
-			SetType<>,
-			SetType<>,
-			SetType<>
-		>();
+				SetType<>,
+				SetType<>,
+				SetType<>,
+				SetType<>
+			>();
 
-		// All same types
-		Test::multipleBinaryAssert<
-			Meta::TypeSet::Difference,
-			
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>
-		>();
+			ASSERT_TRUE(value);
+		}
 
-		// Some same types
-		Test::multipleBinaryAssert<
-			Meta::TypeSet::Difference,
+		{ // All same types
+			constexpr auto value = Test::checkBinaryTypeOperatorMultiple<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 3>, Type< 6>, Type< 7>, Type< 8>>,
-			SetType<Type< 9>, Type< 1>, Type<11>, Type<12>>,
-			SetType<Type<13>, Type<14>, Type< 2>, Type<16>>
-		>();
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>
+			>();
+			
+			ASSERT_TRUE(value);
+		}
 
-		// Some same types all
-		Test::multipleBinaryAssert<
-			Meta::TypeSet::Difference,
+		{ // Some same types
+			constexpr auto value = Test::checkBinaryTypeOperatorMultiple<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 3>, Type< 6>, Type< 7>, Type< 8>>,
-			SetType<Type< 9>, Type< 3>, Type<11>, Type<12>>,
-			SetType<Type<13>, Type<14>, Type< 2>, Type< 3>>
-		>();
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 3>, Type< 6>, Type< 7>, Type< 8>>,
+				SetType<Type< 9>, Type< 1>, Type<11>, Type<12>>,
+				SetType<Type<13>, Type<14>, Type< 2>, Type<16>>
+			>();
 
-		// None same types
-		Test::multipleBinaryAssert<
-			Meta::TypeSet::Difference,
+			ASSERT_TRUE(value);
+		}
+
+		{ // Some same types all
+			constexpr auto value = Test::checkBinaryTypeOperatorMultiple<
+				Meta::TypeSet::Difference,
 			
-			SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
-			SetType<Type< 5>, Type< 6>, Type< 7>, Type< 8>>,
-			SetType<Type< 9>, Type<10>, Type<11>, Type<12>>,
-			SetType<Type<13>, Type<14>, Type<15>, Type<16>>
-		>();
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 3>, Type< 6>, Type< 7>, Type< 8>>,
+				SetType<Type< 9>, Type< 3>, Type<11>, Type<12>>,
+				SetType<Type<13>, Type<14>, Type< 2>, Type< 3>>
+			>();
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // None same types
+			constexpr auto value = Test::checkBinaryTypeOperatorMultiple<
+				Meta::TypeSet::Difference,
+			
+				SetType<Type< 1>, Type< 2>, Type< 3>, Type< 4>>,
+				SetType<Type< 5>, Type< 6>, Type< 7>, Type< 8>>,
+				SetType<Type< 9>, Type<10>, Type<11>, Type<12>>,
+				SetType<Type<13>, Type<14>, Type<15>, Type<16>>
+			>();
+
+			ASSERT_TRUE(value);
+		}
 	}
 
-	TEST(Meta_TypeSet_Difference, Two_Differences) {
+	TEST(Meta_TypeSet_Difference, Two) {
 		#define X(type) twoTest<type>();
 		#include <Test/SetTypes.xmacro>
 	}
 
-	TEST(Meta_TypeSet_Difference, Multiple_Differences) {
+	TEST(Meta_TypeSet_Difference, Multiple) {
 		#define X(type) multipleTest<type>();
 		#include <Test/SetTypes.xmacro>
 	}
--- test_include/Test/Test.hpp
+++ test_include/Test/Test.hpp
@@ -8,34 +8,34 @@
 
 namespace Test {
 	/**
-	 * Asserts that the type Operator<Set1, Set2>::type is the same as @p Correct.
+	 * Checks if the type Operator<Set1, Set2>::type is the same as @p Correct.
 	 * @tparam Operator The operator class.
 	 * @tparam Set1 The first set.
 	 * @tparam Set2 The second set.
 	 * @tparam Correct The expected result type.
+	 * @return The value of the comparison.
 	 */
 	template<template<class...> class Operator, class Set1, class Set2, class Correct>
-	void binaryAssert() {
-		constexpr auto value = std::is_same<
+	constexpr bool checkBinaryTypeOperator() noexcept {
+		return std::is_same<
 			typename Operator<Set1, Set2>::type,
 			Correct
 		>::value;
-
-		ASSERT_TRUE(value);
 	}
 
 	/**
-	 * Asserts that using an operator with multiple arguments has the expected result.
+	 * Checks if an operator with multiple arguments has the expected result.
 	 * The expected result is that the operator is applied as follows: `((Set1 op Set2) op Set3) op Set4`
 	 * @tparam Operator The operator class.
 	 * @tparam Set1 The first set.
 	 * @tparam Set2 The second set.
 	 * @tparam Set3 The third set.
 	 * @tparam Set4 The fourth set.
+	 * @return The value of the comparison.
 	 */
 	template<template<class...> class Operator, class Set1, class Set2, class Set3, class Set4>
-	void multipleBinaryAssert() {
-		constexpr auto value = std::is_same<
+	constexpr bool checkBinaryTypeOperatorMultiple() noexcept {
+		return std::is_same<
 			typename Operator<Set1, Set2, Set3, Set4>::type,
 			typename Operator<
 				typename Operator<
@@ -45,7 +45,5 @@ namespace Test {
 				Set4
 			>::type
 		>::value;
-
-		ASSERT_TRUE(value);
 	}
 }
