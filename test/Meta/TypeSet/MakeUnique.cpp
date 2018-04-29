7d24f874b73090aa59092b25f6f6b0f439e4ee1c
DanielJ <danielwjameson@yahoo.com>
2018-04-29 12:26:32 -0500

    Update MakeUnique tests. #7 #5

--- test/Meta/TypeSet/MakeUnique.cpp
+++ test/Meta/TypeSet/MakeUnique.cpp
@@ -3,56 +3,121 @@
 #include <type_traits>
 
 // Meta
-#include <Meta/TypeSet/MakeUnique.hpp>
 #include <Meta/TypeSet/TypeSet.hpp>
+#include <Meta/TypeSet/MakeUnique.hpp>
 
 // Google Test
 #include <gtest/gtest.h>
 
-#define CREATE_TEST_TYPE(ClassName, Type)\
-	struct ClassName {\
-		using Set1 = Type<int, float, int, bool, int, int, bool, float, int, double, int, int>;\
-		using Unique1 = Type<int, float, bool, double>;\
-		\
-		using Set2 = Type<int, int, int, int>;\
-		using Unique2 = Type<int>;\
-	};\
+// Test
+#include <Test/Type.hpp>
+
 
 namespace {
-	// Define the typed test fixture
-	template<class T>
-	class MetaTypeSetMakeUniqueTest : public testing::Test {
-	};
+	template<template<class...> class Operation, template<class...> class SetType>
+	void singleTest() {
+		{ // Empty
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<>
+				>::type,
+				SetType<>
+			>::value;
 
-	// Defined the classes for the tests.
-	CREATE_TEST_TYPE(TypeSetTest, Meta::TypeSet::TypeSet);
-	CREATE_TEST_TYPE(TupleTest, std::tuple);
+			ASSERT_TRUE(value);
+		}
 
-	// Define the types to use with our typed test
-	using Implementations = testing::Types<
-		TypeSetTest,
-		TupleTest
-	>;
-}
+		{ // No duplicates
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
 
-// Define the tests
-TYPED_TEST_CASE(MetaTypeSetMakeUniqueTest, Implementations);
+			ASSERT_TRUE(value);
+		}
 
-TYPED_TEST(MetaTypeSetMakeUniqueTest, MakeUnique) {
-	{
-		constexpr auto condition = std::is_same<
-			Meta::TypeSet::MakeUnique<TypeParam::Set1>::type,
-			TypeParam::Unique1
-		>::value;
+		{ // One duplicate (1)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<1>, Type<2>, Type<3>, Type<4>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
 
-		ASSERT_TRUE(condition);
+			ASSERT_TRUE(value);
+		}
+
+		{ // One duplicate (2)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<2>, Type<1>, Type<3>, Type<4>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // One duplicate (3)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<2>, Type<3>, Type<1>, Type<4>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // One duplicate (4)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<2>, Type<3>, Type<4>, Type<1>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // One duplicate (5)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<1>, Type<2>, Type<1>, Type<3>, Type<1>, Type<4>, Type<1>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // Multiple duplicate (1)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<1>, Type<1>, Type<2>, Type<2>, Type<3>, Type<3>, Type<4>, Type<4>>
+				>::type,
+				SetType<Type<1>, Type<2>, Type<3>, Type<4>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
+
+		{ // Multiple duplicate (2)
+			constexpr auto value = std::is_same<
+				Operation<
+					SetType<Type<4>, Type<1>, Type<3>, Type<2>, Type<2>, Type<3>, Type<1>, Type<4>>
+				>::type,
+				SetType<Type<4>, Type<1>, Type<3>, Type<2>>
+			>::value;
+
+			ASSERT_TRUE(value);
+		}
 	}
-	{
-		constexpr auto condition = std::is_same<
-			Meta::TypeSet::MakeUnique<TypeParam::Set2>::type,
-			TypeParam::Unique2
-		>::value;
 
-		ASSERT_TRUE(condition);
+	TEST(Meta_TypeSet_MakeUnique, Single) {
+		#define X(type) singleTest<Meta::TypeSet::MakeUnique, type>();
+		#include <Test/SetTypes.xmacro>
 	}
 }
